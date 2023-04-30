569a2747b90b027de5ed40a5c9fe99465bdab0d5
Dylan <dylanoconnor32@gmail.com>
2023-04-30 17:45:59 +0100

    Initial Commit

---
+++ Folio/FolioApp.swift
@@ -0,0 +1,17 @@
+//
+//  FolioApp.swift
+//  Folio
+//
+//  Created by Dylan O'Connor on 30/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct FolioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
